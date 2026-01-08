06a046c15dd4b702830f029a87d6916e8b982186
Abilio Soares Coelho <abiliocoelho@gmail.com>
2026-01-08 17:40:18 -0300

    Add react-native-reanimated-carousel dependency and integrate carousel in Index component for enhanced UI experience

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -5,9 +5,16 @@ import {
   ActivityIndicator,
   AppState,
   AppStateStatus,
+  Dimensions,
+  Image,
   Text,
   View,
 } from "react-native";
+import { useSharedValue } from "react-native-reanimated";
+import Carousel, {
+  ICarouselInstance,
+  Pagination,
+} from "react-native-reanimated-carousel";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import io, { Socket } from "socket.io-client";
 
@@ -27,7 +34,35 @@ type Origin = {
   updated: string;
 };
 
+const width = Dimensions.get("window").width;
+
+const ref = useRef<ICarouselInstance>(null);
+const progress = useSharedValue<number>(0);
+
+const onPressPagination = (index: number) => {
+  ref.current?.scrollTo({
+    count: index - progress.value,
+    animated: true,
+  });
+};
+
 const SOCKET_URL = "https://api.abiliocoelho.dev";
+
+const data = [
+  {
+    url: "https://bond.app.br/bond/anuncie.png",
+    descricao: "Anuncie aqui",
+  },
+
+  {
+    url: "https://bond.app.br/bond/anuncie.png",
+    descricao: "Anuncie aqui",
+  },
+  {
+    url: "https://bond.app.br/bond/anuncie.png",
+    descricao: "Anuncie aqui",
+  },
+];
 export default function Index() {
   const [origin, setOrigin] = useState<Origin | null>(null);
   const [loading, setLoading] = useState(true);
@@ -95,7 +130,7 @@ export default function Index() {
 
   return (
     <View
-      className="flex-1 gap-4 bg-background p-4"
+      className="flex-1 gap-2 bg-background p-4 justify-between"
       style={{ paddingTop: top, paddingBottom: bottom }}
     >
       {loading ? (
@@ -104,31 +139,92 @@ export default function Index() {
         </View>
       ) : (
         <>
-          <View className="flex items-center justify-between p-4 bg-card rounded-lg">
-            <Text className="text-foreground">
-              {origin?.origin === "Teresina"
-                ? "Verde em Teresina"
-                : origin?.origin === "Timon"
-                  ? "Verde em Timon"
-                  : origin?.origin === "Stop"
-                    ? "Ponte fechada"
-                    : "Passagem de trem"}
+          <View>
+            <Text className="text-foreground text-lg font-bold text-center">
+              Ponte João Luís Ferreira
             </Text>
-            {origin?.lastFlow && (
-              <Text className="text-foreground text-xs">
-                {origin.lastFlow.origin}
+            <View className="flex items-center justify-between p-4 bg-card rounded-lg">
+              <Text className="text-foreground">
+                {origin?.origin === "Teresina"
+                  ? "Verde em Teresina"
+                  : origin?.origin === "Timon"
+                    ? "Verde em Timon"
+                    : origin?.origin === "Stop"
+                      ? "Ponte fechada"
+                      : "Passagem de trem"}
               </Text>
-            )}
-            <Text className="text-foreground/50 text-xl font-bold">
-              {formatTime(elapsedTime)}
-            </Text>
+
+              <Text className="text-foreground/50 text-xl font-bold">
+                {formatTime(elapsedTime)}
+              </Text>
+              {origin?.lastFlow && (
+                <View className="border-t border-foreground/50 py-2 w-full mt-2">
+                  <View>
+                    <Text className="text-foreground text-base font-bold text-center">
+                      Último fluxo aberto
+                    </Text>
+                    <View className="flex-row items-center justify-between">
+                      <View>
+                        <Text className="text-foreground text-sm">Direção</Text>
+                        <Text className="text-foreground/50 text-sm font-bold">
+                          {origin.lastFlow.origin === "Teresina"
+                            ? "Teresina → Timon"
+                            : "Timon → Teresina"}
+                        </Text>
+                      </View>
+                      <View>
+                        <Text className="text-foreground text-sm">Duração</Text>
+                        <Text className="text-foreground/50 text-sm font-bold">
+                          {formatTime(origin.lastFlow.duration)}
+                        </Text>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+              )}
+            </View>
+            <View className="flex-row gap-2 items-center justify-center">
+              <Lighthouse
+                city="Teresina"
+                isOpen={origin?.origin === "Teresina"}
+              />
+              <Lighthouse city="Timon" isOpen={origin?.origin === "Timon"} />
+            </View>
           </View>
-          <View className="flex-row gap-2 items-center justify-center">
-            <Lighthouse
-              city="Teresina"
-              isOpen={origin?.origin === "Teresina"}
+          <View className="w-full">
+            <Carousel
+              ref={ref}
+              autoPlay={true}
+              loop={true}
+              width={width * 0.95}
+              height={(width * 0.95) / 2}
+              data={data}
+              onProgressChange={progress}
+              renderItem={({ item }) => (
+                <View className="border-radius-8 flex-1 overflow-hidden rounded-lg">
+                  <Image
+                    source={{ uri: item.url }}
+                    className="h-full w-full"
+                    resizeMode="cover"
+                  />
+                </View>
+              )}
             />
-            <Lighthouse city="Timon" isOpen={origin?.origin === "Timon"} />
+
+            <Pagination.Basic
+              progress={progress}
+              data={data}
+              dotStyle={{
+                backgroundColor: colors.foreground,
+                borderRadius: 10,
+              }}
+              activeDotStyle={{
+                backgroundColor: colors.primary,
+                borderRadius: 10,
+              }}
+              containerStyle={{ gap: 5, marginTop: 10 }}
+              onPress={onPressPagination}
+            />
           </View>
         </>
       )}
